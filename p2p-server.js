045cfb3a8ef3b885de38deaa8ee85ed42126da12
chalee <36904775+PrettyKing@users.noreply.github.com>
2025-05-19 14:28:49 +0800

    添加P2P网络服务器实现

---
+++ p2p-server.js
@@ -0,0 +1,80 @@
+// p2p-server.js - 区块链P2P网络服务器
+const WebSocket = require('ws');
+const config = require('./config');
+
+class P2pServer {
+  constructor(blockchain) {
+    this.blockchain = blockchain;
+    this.sockets = [];
+  }
+
+  // 初始化P2P服务器
+  listen() {
+    const server = new WebSocket.Server({ port: config.P2P_PORT });
+    server.on('connection', socket => this.connectSocket(socket));
+    
+    this.connectToPeers();
+    
+    console.log(`监听P2P连接，端口: ${config.P2P_PORT}`);
+  }
+
+  // 连接到初始节点
+  connectToPeers() {
+    config.PEERS.forEach(peer => {
+      const socket = new WebSocket(peer);
+      socket.on('open', () => this.connectSocket(socket));
+    });
+  }
+
+  // 处理新连接
+  connectSocket(socket) {
+    this.sockets.push(socket);
+    console.log('Socket connected');
+    
+    this.messageHandler(socket);
+    this.sendChain(socket);
+  }
+
+  // 消息处理
+  messageHandler(socket) {
+    socket.on('message', message => {
+      const data = JSON.parse(message);
+      
+      switch(data.type) {
+        case 'CHAIN':
+          this.blockchain.replaceChain(data.chain);
+          break;
+        case 'TRANSACTION':
+          this.blockchain.addTransaction(data.transaction);
+          break;
+      }
+    });
+  }
+
+  // 发送区块链数据
+  sendChain(socket) {
+    socket.send(JSON.stringify({ 
+      type: 'CHAIN', 
+      chain: this.blockchain.chain 
+    }));
+  }
+
+  // 广播区块链更新
+  syncChains() {
+    this.sockets.forEach(socket => {
+      this.sendChain(socket);
+    });
+  }
+
+  // 广播交易
+  broadcastTransaction(transaction) {
+    this.sockets.forEach(socket => {
+      socket.send(JSON.stringify({
+        type: 'TRANSACTION',
+        transaction
+      }));
+    });
+  }
+}
+
+module.exports = P2pServer;
